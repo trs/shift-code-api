ebb11c1c418c9084637de47220e303453fa86fad
Tyler Stewart <hello@tylerstewart.ca>
2019-10-10 20:26:23 -0600

    feat: simple cli

---
+++ src/bin.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import {login, redeem} from './index';
+
+(async () => {
+  const [,, email, password, code] = process.argv;
+
+  const session = await login({
+    email,
+    password
+  });
+
+  const results = await redeem(session, code);
+
+  console.log(results);
+  process.exit(0);
+})()
+  .catch((err) => {
+    console.error(err.message);
+    process.exit(1);
+  });
